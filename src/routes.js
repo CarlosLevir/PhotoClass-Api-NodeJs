5f1f9bf6396f37e4be5fbc61f14af03bd556145b
Carlos Levir Freire de Souza <carlos.levir@secrelnet.local>
2019-04-29 15:12:06 -0300

    Routes Config.

--- src/routes.js
+++ src/routes.js
@@ -4,12 +4,19 @@ const multerConfig = require('./config/multer');
 
 const SubjectController = require('./controllers/SubjectController');
 const FileController = require('./controllers/FileController');
+const UserController = require('./controllers/UserController');
 
 const routes = express.Router();
 
-routes.post('/subjects', SubjectController.store);
-routes.get('/subjects/:id', SubjectController.show);
+// User routes
+routes.post('/user', UserController.store);
+routes.get('/user/:id', UserController.show);
 
-routes.post('/subjects/:id/files', multer(multerConfig).single('file'), FileController.store);
+// Subject routes
+routes.post('/subject/:userId', SubjectController.store);
+routes.get('/subject/:id', SubjectController.show);
+
+// Files routes
+routes.post('/subject/:id/files', multer(multerConfig).single('file'), FileController.store);
 
 module.exports = routes;
