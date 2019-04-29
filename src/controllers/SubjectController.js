2702b4a5ff27ff3a636fff439cb903f2a50cb655
Carlos Levir Freire de Souza <carlos.levir@secrelnet.local>
2019-04-29 15:13:07 -0300

    Push and Emit in User Subjects.

--- src/controllers/SubjectController.js
+++ src/controllers/SubjectController.js
@@ -1,8 +1,19 @@
 const Subject = require('../models/Subject');
+const User = require('../models/User');
 
 class SubjectController {
   async store(req, res) {
-    const subject = await Subject.create({ title: req.body.title });
+    const user = await User.findById(req.params.userId);
+    const subject = await Subject.create({
+      title: req.body.title,
+      userId: req.body.userId
+    });
+
+    user.subjects.push(subject);
+
+    await user.save();
+
+    req.io.sockets.in(user._id).emit('subject', subject);
 
     return res.json(subject);
   }
